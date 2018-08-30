6712f242e1cad728856b6c9f51c7369243442225
shadow-light <42055707+shadow-light@users.noreply.github.com>
2018-08-30 15:25:46 +1000

    tmp check

--- npmenv_test.py
+++ npmenv_test.py
@@ -308,7 +308,7 @@ def test_env_run(sandbox, capfd):
     with pytest.raises(npmenv.NpmenvException):
         npmenv.env_run('node --version')
     # Confirm runs executable from .bin dir
-    result = npmenv.env_npm('install "node@10.4.1"')  # Specific version to avoid system version
+    result = npmenv.env_npm('install "node@10.4.1"')  # Specific version to avoid system
     result.check_returncode()
     capfd.readouterr()
     npmenv.env_run('node --version')
