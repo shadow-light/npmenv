2faa6f0fb417dcdab9e93e6579cda2a0de36f6fc
shadow-light <42055707+shadow-light@users.noreply.github.com>
2018-09-03 13:29:38 +1000

    fix for prev commit

--- dev.py
+++ dev.py
@@ -148,7 +148,7 @@ def release(inv):
 
     # Helper to get git stdout
     def git_out(cmd):
-        result = inv.run(f'git {cmd}', warn=True, pty=False, echo=True)
+        result = inv.run(f'git {cmd}', warn=True, pty=False, hide='both')
         if result.failed:
             return None
         return result.stdout.strip()
