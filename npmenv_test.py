730a8f6a4958cbe24912db40ee893abacb90a9df
shadow-light <42055707+shadow-light@users.noreply.github.com>
2018-09-03 13:35:18 +1000

    fix test for windows

--- npmenv_test.py
+++ npmenv_test.py
@@ -109,7 +109,6 @@ def fake_project():
         data_dir = 'Library/Application Support'
     if platform.system() == 'Windows':
         proj_dir = 'C:' + proj_dir
-        env_id = 'TEMP__fake__60Sq7Ynp'
         data_dir = 'AppData/Local/shadow-light'
     return {
         'proj_dir': Path(proj_dir),
